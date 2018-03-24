8e90eb18f1524468e92da2ff89685fd265c56f00
Isaac Chen <isaacchen@isaacchen.cn>
2018-03-24 15:11:12 +0100

    santoni: Update for lineage vendor change
    Signed-off-by: Isaac Chen <isaacchen@isaacchen.cn>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -20,30 +20,7 @@ set -e
 
 DEVICE=santoni
 VENDOR=xiaomi
-
+DEVICE_COMMON=msm8937-common
 INITIAL_COPYRIGHT_YEAR=2018
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
-
-CM_ROOT="$MY_DIR"/../../..
-
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
-    exit 1
-fi
-. "$HELPER"
-
-# Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
-
-# Copyright headers and guards
-write_headers
-
-# Device blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt
-
-# Finish
-write_footers
+./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
